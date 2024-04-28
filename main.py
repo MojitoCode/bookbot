f15e89fd6ef505a51c887bbfbe1970c73a127fbe
zomb37 <howlingnights@gmail.com>
2024-04-28 01:28:33 -0700

    updating functions in main

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ def main():
     num_of_words = len(words)
     print(f"There were {num_of_words} words found in the: {book_path} document.")
 
+def count_letters():
+    book_chars = {}
+    book_path = "./books/frankenstein.txt" 
+    with open(book_path) as f:
+        for i in f:
+            lower_string = i.lower()
+            if lower_string in book_chars:
+                book_chars[lower_string] +=1
+            else:
+                book_chars[lower_string] = 1
+    print(lower_string)
+
 main()
+count_letters()
