94917cbbcc5ed3330c8208c056fabde9aa406f26
zomb37 <howlingnights@gmail.com>
2024-04-28 01:02:42 -0700

    created a main function to return the number of words in a given document

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def main(){
-
-with open("~/workspace/github.com/zomb37/bookbot/books/frankenstein.txt") as f:
-    file_contents = f.read()
-}
+def main():
+    book_path = "./books/frankenstein.txt" 
+    with open(book_path) as f:
+        file_contents = f.read()
+    
+    words = file_contents.split()
+    num_of_words = len(words)
+    print(f"There were {num_of_words} words found in the: {book_path} document.")
 
 main()
